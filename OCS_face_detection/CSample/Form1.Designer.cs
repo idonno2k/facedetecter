20cb1b833ebed92d342d959b68d0e5e177c51d10
박병훈 <idonno2000@gmail.com>
2017-07-26 22:05:03 +0900

    얼굴찾아서 얼굴만 잘라주는 프로그램

---
+++ OCS_face_detection/CSample/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxIpl2 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIpl2
+            // 
+            this.pictureBoxIpl2.Location = new System.Drawing.Point(13, 13);
+            this.pictureBoxIpl2.Name = "pictureBoxIpl2";
+            this.pictureBoxIpl2.Size = new System.Drawing.Size(683, 442);
+            this.pictureBoxIpl2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            //this.pictureBoxIpl2.Size = new Size(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            this.pictureBoxIpl2.TabIndex = 0;
+            this.pictureBoxIpl2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(563, 470);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 516);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBoxIpl2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OCS_face_detection/CSample/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+using OpenCvSharp;
+using OpenCvSharp.Test;
+
+
+namespace CSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //return;
+        }
+
+        class Face : IDisposable
+        {
+            IplImage FindFace;
+            public IplImage FaceDetect( string filepath)
+            {
+                // CvHaarClassifierCascade, cvHaarDetectObjects
+                // 얼굴을 검출하기 위해서 Haar 분류기의 캐스케이드를 이용한다
+                CvColor[] colors = new CvColor[]{
+                new CvColor(0,0,255),
+                new CvColor(0,128,255),
+                new CvColor(0,255,255),
+                new CvColor(0,255,0),
+                new CvColor(255,128,0),
+                new CvColor(255,255,0),
+                new CvColor(255,0,0),
+                new CvColor(255,0,255),
+            };
+                const double scale = 1.04;
+                const double scaleFactor = 1.139;
+                const int minNeighbors = 2;
+
+                IplImage src = new IplImage(filepath, LoadMode.Color);
+
+                using (IplImage img = src.Clone())
+                using (IplImage smallImg = new IplImage(new CvSize(Cv.Round(img.Width / scale), Cv.Round(img.Height / scale)), BitDepth.U8, 1))
+                {
+                    // 얼굴 검출용의 화상의 생성
+                    using (IplImage gray = new IplImage(img.Size, BitDepth.U8, 1))
+                    {
+                        Cv.CvtColor(img, gray, ColorConversion.BgrToGray);
+                        Cv.Resize(gray, smallImg, Interpolation.Linear);
+                        Cv.EqualizeHist(smallImg, smallImg);
+                    }
+                    using (CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile(Application.StartupPath + "\\" + "haarcascade_frontalface_alt.xml"))
+
+                    using (CvMemStorage storage = new CvMemStorage())
+                    {
+                        storage.Clear();
+                        // 얼굴의 검출
+                        CvSeq<CvAvgComp> faces = Cv.HaarDetectObjects(smallImg, cascade, storage, scaleFactor, minNeighbors, 0, new CvSize(120, 120));
+                        // 검출한 얼굴에사각형을 그린다
+                        for (int i = 0; i < faces.Total; i++)
+                        {
+                            CvRect r = faces[i].Value.Rect;
+                            CvPoint center = new CvPoint
+                            {
+                                X = Cv.Round((r.X + r.Width * 0.5) * scale),
+                                Y = Cv.Round((r.Y + r.Height * 0.5) * scale)
+                            };
+                            int radius = Cv.Round((r.Width + r.Height)*1.0);
+                           // img.Circle(center, radius, colors[i % 8], 15, LineType.AntiAlias, 0);
+
+                            r.X = center.X - Cv.Round(radius * 0.5);
+                            r.Y = center.Y - Cv.Round(radius * 0.5);
+
+                            r.Width = radius;
+                            r.Height = radius; 
+
+                           // img.Rectangle(r, colors[i % 8],1,LineType.AntiAlias, 0);
+
+                            using (IplImage crobImg = src.Clone())
+                            {
+                                Cv.Resize(img, crobImg, Interpolation.Linear);
+                                Cv.SetImageROI(crobImg, r);
+                                // Cv.ShowImage("crop",crobImg);
+                           
+                                string savePath = Path.GetDirectoryName(filepath);
+                                string savefile = Path.GetFileNameWithoutExtension(filepath);
+
+                               // savePath = savePath + "\\crobimg";
+                               // DirectoryInfo di = new DirectoryInfo(savePath);
+                               // if (di.Exists == false)
+                               // {
+                               //     di.Create();
+                               // }
+
+                                string savefilename = savePath + "\\crobimg\\" + savefile + "_" + i + ".jpg";
+                                Cv.SaveImage(savefilename, crobImg); 
+                            }
+                        }
+                    }
+                    IplImage testFindFace = img.Clone();
+                    return testFindFace;
+                }
+            }
+
+            public void Dispose()
+            {
+                if (FindFace != null) FindFace.Dispose();
+            }
+        }
+
+        private string m_folder_path;
+        static string[] m_Files;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog forderbrowser = new FolderBrowserDialog();
+            if (forderbrowser.ShowDialog() == DialogResult.OK)
+            {
+                m_folder_path = forderbrowser.SelectedPath;
+                m_Files = Directory.GetFiles(m_folder_path,"*.jpg");
+
+                if (m_Files != null)
+                {
+                    string sDirPath;
+                    sDirPath = m_folder_path + "\\crobimg";
+                    DirectoryInfo di = new DirectoryInfo(sDirPath);
+                    if (di.Exists == false)
+                    {
+                        di.Create();
+                    }
+
+                    Thread t1 = new Thread(new ThreadStart(threadFaceDetectionRun));
+                    t1.Start();
+                }
+            }
+
+        }
+
+
+        private void threadFaceDetectionRun()
+        {
+            
+            for( int i=0; i < m_Files.Length ;i++)
+            {
+                //IplImage img = new IplImage(m_folder_path + m_Files[i], LoadMode.Color);
+
+                Face fac = new Face();
+                IplImage imgHarr = fac.FaceDetect( m_Files[i]);
+
+                pictureBoxIpl2.ImageIpl = imgHarr;
+            }
+        }
+
+
+    }
+}
